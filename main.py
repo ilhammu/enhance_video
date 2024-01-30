aa1f5bbc86daf434dd782edaf4d9a77fa1a52beb
ilhammu <86983014+ilhammu@users.noreply.github.com>
2024-01-30 14:34:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,45 +3,52 @@ import streamlit as st
 import numpy as np
 import tempfile
 import os
-import requests
 
 def enhance_video(input_file, output_file):
+    # Buka file video
     cap = cv2.VideoCapture(input_file)
 
+    # Periksa apakah video berhasil dibuka
     if not cap.isOpened():
         st.error("Gagal membuka video")
         return
 
+    # Dapatkan properti video
     width = int(cap.get(3))
     height = int(cap.get(4))
 
+    # Buat objek VideoWriter untuk menyimpan video hasil perbaikan
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(output_file, fourcc, 20.0, (width, height))
 
     while cap.isOpened():
+        # Baca frame dari video
         ret, frame = cap.read()
 
         if not ret:
             break
 
+        # Proses frame untuk meningkatkan kualitas (contoh: flip vertikal)
         enhanced_frame = cv2.flip(frame, 0)
 
+        # Tulis frame yang telah ditingkatkan ke file video output
         out.write(enhanced_frame)
 
+        # Tampilkan frame yang telah ditingkatkan
+        st.image(enhanced_frame, channels="BGR")
+
+    # Tutup file video input dan output
     cap.release()
     out.release()
 
-def upload_to_tmpfiles(video_path):
-    url = "https://tmpfiles.org/api/v1/upload"
-    files = {'file': open(video_path, 'rb')}
-    response = requests.post(url, files=files)
-    return response.json()['url']
+    # Tampilkan tautan unduh setelah pemrosesan selesai
+    st.success(f"Video telah ditingkatkan. Unduh di sini: [Download Video]({output_file})")
 
 if __name__ == "__main__":
     st.title("Enhance Video App")
 
     uploaded_file = st.file_uploader("Pilih video untuk ditingkatkan", type=["mp4", "avi"])
-    st.info('Maksimal video yang dapat diunggah 100MB')
+
     if uploaded_file is not None:
         # Simpan video sementara sebagai file lokal
         with tempfile.NamedTemporaryFile(delete=False) as temp_file:
@@ -56,11 +63,3 @@ if __name__ == "__main__":
 
         # Hapus file sementara setelah video ditingkatkan
         os.remove(video_path)
-
-        st.success("Video telah ditingkatkan.")
-        
-        # Unggah video yang telah ditingkatkan ke tmpfiles.org
-        enhanced_video_url = upload_to_tmpfiles(output_file)
-
-        # Tampilkan video yang telah ditingkatkan dari tmpfiles.org
-        st.video(enhanced_video_url)
