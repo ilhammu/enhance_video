eaf2a0f44daa9fdd07c539013e2ba636082a96c3
ilhammu <86983014+ilhammu@users.noreply.github.com>
2024-01-30 14:40:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 import streamlit as st
 import numpy as np
+import tempfile
+import os
 
 def enhance_video(input_file, output_file):
     cap = cv2.VideoCapture(input_file)
@@ -12,11 +14,7 @@ def enhance_video(input_file, output_file):
     width = int(cap.get(3))
     height = int(cap.get(4))
 
-    # Ganti ekstensi file output sesuai dengan format yang diinginkan (.mp4)
-    output_file = output_file.replace('.avi', '.mp4')
-
-    # Ganti nilai fourcc sesuai dengan format video MP4
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(output_file, fourcc, 20.0, (width, height))
 
     while cap.isOpened():
@@ -38,9 +36,21 @@ if __name__ == "__main__":
     uploaded_file = st.file_uploader("Pilih video untuk ditingkatkan", type=["mp4", "avi"])
 
     if uploaded_file is not None:
-        st.video(uploaded_file)
+        # Simpan video sementara sebagai file lokal
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(uploaded_file.read())
+            video_path = temp_file.name
+
+        # Tampilkan video yang diunggah
+        st.video(video_path)
 
         output_file = 'enhanced_video.avi'
-        enhance_video(uploaded_file, output_file)
+        enhance_video(video_path, output_file)
 
-        st.success(f"Video telah ditingkatkan. Silakan unduh [di sini](enhanced_video.mp4).")
+        # Hapus file sementara setelah video ditingkatkan
+        os.remove(video_path)
+
+        st.success("Video telah ditingkatkan.")
+        
+        # Tampilkan video yang telah ditingkatkan di bawah pesan sukses
+        st.video(output_file)
